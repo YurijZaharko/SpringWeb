775c8b57d84b048eb32a83a4c36ba3ce3c37f15f
YurijZaharko <ZaharkoYurij@gmail.com>
2016-10-01 16:20:10 +0300

    1

---
+++ src/main/java/proj/service/MailSender.java
@@ -0,0 +1,52 @@
+package proj.service;
+
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+/**
+ * Created by SC on 01.10.2016.
+ */
+@Service
+public class MailSender {
+
+    private final static String USERNAME = "zaharkoyurij@gmail.com";
+    private final static String PASSWORD = "jnwoHW612+7312FKsdf=";
+    @Async
+    public void sendMail(String content, String email, String mailBody) {
+        Properties properties = System.getProperties();
+        properties.setProperty("mail.smtp.starttls.enable", "true");
+        properties.setProperty("mail.smtp.auth", "true");
+        properties.setProperty("mail.smtp.port", "465");
+        properties.setProperty("mail.smtp.host", "smtp.gmail.com");
+        properties.setProperty("mail.smtp.socketFactory.port", "465");
+        properties.setProperty("mail.smtp.socketFactory.class",
+                "javax.net.ssl.SSLSocketFactory");
+        Session session = Session.getDefaultInstance(properties,
+                new Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(USERNAME, PASSWORD);
+                    }
+                });
+        try {
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(USERNAME));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(
+                    USERNAME));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(
+                    email));
+            message.setSubject(content, "UTF-8");
+            message.setText(mailBody);
+            synchronized (this) {
+                Transport.send(message);
+            }
+        } catch (MessagingException mex) {
+            mex.printStackTrace();
+            System.out.println("You have some problems with connection!");
+        }
+    }
+}
