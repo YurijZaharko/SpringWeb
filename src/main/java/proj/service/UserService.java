fb45f03a60e3556e58d974af268770197cbffa76
YurijZaharko <ZaharkoYurij@gmail.com>
2017-01-13 16:42:01 +0200

    Add method Page<User> findAll

--- src/main/java/proj/service/UserService.java
+++ src/main/java/proj/service/UserService.java
@@ -1,6 +1,9 @@
 package proj.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import proj.entity.User;
+import proj.form.Filter.UserFilterForm;
 
 /**
  * Created by SCIP on 20.09.2016.
@@ -9,6 +12,8 @@ public interface UserService {
     User findByLogin(String login);
 
     User findById(int id);
+
+    Page<User> findAll(Pageable pageable, UserFilterForm userFilterForm);
 
     void save(User user);
 }
