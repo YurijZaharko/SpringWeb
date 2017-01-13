fe6e72451548fe3fc53ef8fa88fcd5cc6dc92e5a
YurijZaharko <ZaharkoYurij@gmail.com>
2017-01-13 16:31:21 +0200

    Change import

--- src/main/java/proj/entity/User.java
+++ src/main/java/proj/entity/User.java
@@ -10,7 +10,6 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.Objects;
 
 /**
  * Created by SCIP on 14.08.2016.
