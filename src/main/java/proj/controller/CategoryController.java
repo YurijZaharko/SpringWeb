624798b29e63552611a68d82bd07192cdab7a96f
YurijZaharko <ZaharkoYurij@gmail.com>
2017-08-21 16:20:23 +0300

    --REF-002

--- src/main/java/proj/controller/CategoryController.java
+++ src/main/java/proj/controller/CategoryController.java
@@ -26,12 +26,25 @@ import javax.validation.Valid;
  * Created by SCIP on 16.08.2016.
  */
 @Controller
+@RequestMapping("/admin")
 public class CategoryController {
-    @Autowired
-    private CategoryService categoryService;
+
+    private final CategoryService categoryService;
+
+    private final StringPropertiesService stringPropertiesService;
+
+    private static final String REDIRECT = "redirect:/admin/adminCategory";
+    private static final String ADMIN_CATEGORY = "adminCategory";
+    private static final String SEPERATOR = "/";
+    private static final String CATEGORIES = "categories";
+    
 
     @Autowired
-    private StringPropertiesService stringPropertiesService;
+    public CategoryController(CategoryService categoryService, StringPropertiesService stringPropertiesService) {
+        this.categoryService = categoryService;
+        this.stringPropertiesService = stringPropertiesService;
+    }
+
 
     @ModelAttribute("category")
     public Category getCategory(){
@@ -50,16 +63,16 @@ public class CategoryController {
         webDataBinder.registerCustomEditor(StringProperties.class, new StrPropEditor(stringPropertiesService));
     }
 
-    @RequestMapping("/admin/adminCategory")
+    @RequestMapping("/adminCategory")
     public String showCategory(Model model,
                                @PageableDefault(5) Pageable pageable,
                                @ModelAttribute("filter") CategoryFilterForm categoryFilterForm){
-        model.addAttribute("categories", categoryService.findAll(pageable, categoryFilterForm));
+        model.addAttribute(CATEGORIES, categoryService.findAll(pageable, categoryFilterForm));
         model.addAttribute("rootCategories", categoryService.findByRootCategoryTrue());
-        return "adminCategory";
+        return ADMIN_CATEGORY;
     }
 
-    @RequestMapping("/admin/adminCategory/categoryWithProperty/{id}")
+    @RequestMapping(SEPERATOR + ADMIN_CATEGORY + "/categoryWithProperty/{id}")
     public String showCategoryWithStringProperty(Model model,
                                                  @PathVariable int id,
                                                  @PageableDefault(5) Pageable pageable,
@@ -71,56 +84,56 @@ public class CategoryController {
 
 
     @Transactional
-    @RequestMapping(value = "/admin/adminCategory/categoryWithProperty/propertyId/{catId}/{id}")
+    @RequestMapping(value = SEPERATOR + ADMIN_CATEGORY + "/categoryWithProperty/propertyId/{catId}/{id}")
     public String savePropertyToCategory(@PathVariable("catId") int catId ,
                                          @PathVariable("id") int id,
                                          @PageableDefault(5) Pageable pageable,
                                          @ModelAttribute("filter") CategoryFilterForm categoryFilterForm){
         categoryService.savePropertyToCategory(pageable, categoryFilterForm, catId, id);
-        return "redirect:/admin/adminCategory/categoryWithProperty/" + catId + CommonMethod.getParams(pageable, categoryFilterForm);
+        return REDIRECT + "/categoryWithProperty/" + catId + CommonMethod.getParams(pageable, categoryFilterForm);
     }
 
     @Transactional
-    @RequestMapping(value = "/admin/adminCategory/categoryWithProperty/removePropertyId/{catId}/{id}")
+    @RequestMapping(value = SEPERATOR + ADMIN_CATEGORY + "/categoryWithProperty/removePropertyId/{catId}/{id}")
     public String removePropertyFromCategory(@PathVariable("catId") int catId ,
                                          @PathVariable("id") int id,
                                          @PageableDefault(5) Pageable pageable,
                                          @ModelAttribute("filter") CategoryFilterForm categoryFilterForm){
         categoryService.removePropertyFromCategory(pageable, categoryFilterForm, catId, id);
-        return "redirect:/admin/adminCategory/categoryWithProperty/" + catId + CommonMethod.getParams(pageable, categoryFilterForm);
+        return REDIRECT + "/categoryWithProperty/" + catId + CommonMethod.getParams(pageable, categoryFilterForm);
     }
 
-    @RequestMapping(value = "/admin/adminCategory", method = RequestMethod.POST)
+    @RequestMapping(value = SEPERATOR + ADMIN_CATEGORY , method = RequestMethod.POST)
     public String save(@ModelAttribute("category") @Valid Category category,
                        BindingResult bindingResult,
                        @PageableDefault(5) Pageable pageable,
                        @ModelAttribute("filter") CategoryFilterForm categoryFilterForm,
                        Model model){
         if(bindingResult.hasErrors()){
-            model.addAttribute("categories", categoryService.findAll(pageable, categoryFilterForm));
-            return "adminCategory";
+            model.addAttribute(CATEGORIES, categoryService.findAll(pageable, categoryFilterForm));
+            return ADMIN_CATEGORY;
         }
         categoryService.save(category);
-        return "redirect:/admin/adminCategory" + CommonMethod.getParams(pageable, categoryFilterForm);
+        return REDIRECT + CommonMethod.getParams(pageable, categoryFilterForm);
     }
 
     @Modifying
     @Transactional
-    @RequestMapping("/admin/adminCategory/delete/{id}")
+    @RequestMapping(SEPERATOR + ADMIN_CATEGORY +"/delete/{id}")
     public String deleteCategory(@PathVariable int id,
                               @PageableDefault(5) Pageable pageable,
                               @ModelAttribute("filter") CategoryFilterForm categoryFilterForm){
         categoryService.deleteById(id);
-        return "redirect:/admin/adminCategory" + CommonMethod.getParams(pageable, categoryFilterForm);
+        return REDIRECT + CommonMethod.getParams(pageable, categoryFilterForm);
     }
 
-    @RequestMapping("/admin/adminCategory/update/{id}")
+    @RequestMapping(SEPERATOR + ADMIN_CATEGORY + "/update/{id}")
     public String updateCategory(@PathVariable int id, Model model,
                                  @PageableDefault(5) Pageable pageable,
                                  @ModelAttribute("filter") CategoryFilterForm categoryFilterForm){
         model.addAttribute("category", categoryService.findByIdFetchParentId(id));
-        model.addAttribute("categories", categoryService.findAll(pageable, categoryFilterForm));
+        model.addAttribute(CATEGORIES, categoryService.findAll(pageable, categoryFilterForm));
         model.addAttribute("rootCategories", categoryService.findByRootCategoryTrue());
-        return "adminCategory";
+        return ADMIN_CATEGORY;
     }
 }
