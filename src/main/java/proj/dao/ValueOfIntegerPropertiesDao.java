1ab2bb20706bc368ce7cf41b532670f7ed8ecf07
YurijZaharko <ZaharkoYurij@gmail.com>
2017-08-21 11:34:42 +0300

    --refactoring

--- src/main/java/proj/dao/ValueOfIntegerPropertiesDao.java
+++
@@ -1,9 +0,0 @@
-package proj.dao;
-
-import proj.entity.ValueOfIntegerProperties;
-
-/**
- * Created by SCIP on 31.07.2016.
- */
-public interface ValueOfIntegerPropertiesDao extends GenericDao<ValueOfIntegerProperties, Integer> {
-}
